1372375176341663e5274d2a06343ba7dc1f9991
hugo-wb <hugowong71@gmail.com>
2021-02-05 00:54:26 +0000

    successfully turned into PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
+console.log("ENV:" + process.env.NODE_ENV);
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
+    disable: process.env.NODE_ENV === "development",
+    dest: "public",
     runtimeCaching,
   },
-})
+});
