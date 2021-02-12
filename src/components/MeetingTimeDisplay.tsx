4015ae65cc5f509aaba846ccb87ae61f593cfb2c
EdwardW <02ewatine@gmail.com>
2021-02-12 01:12:06 +0000

    Add a modal to show multiple meeting times
    Add a modal on MeetingTimeDisplay
    BUG: There is a non-breaking bug related to keys on list items
    Add a function to stringConverters for time ranges
    Change componentStyles to change the mouse pointer on links

--- src/components/MeetingTimeDisplay.tsx
+++ src/components/MeetingTimeDisplay.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { MeetingTime } from "../firestoreTypes";
+import { MeetingTime, Day, Frequency } from "../firestoreTypes";
 import { FaUserClock } from "react-icons/fa"
 import TextWithIcon from "./TextWithIcon";
-import { convertMeetingTime } from "../stringConverters";
+import { convertMeetingTime, convertTimeRange } from "../stringConverters";
 import styles from "./componentStyles.module.css";
 import {
     Modal,
@@ -13,7 +13,11 @@ import {
     ModalBody,
     ModalCloseButton,
     useDisclosure,
-  } from "@chakra-ui/react"
+    VStack,
+    Text,
+    UnorderedList,
+    ListItem
+  } from "@chakra-ui/react";
 
 interface Props {
   meetingTimes?: MeetingTime[]; // Use for multiple times (has priority)
@@ -32,22 +36,53 @@ const MeetingTimeDisplay = (props: Props) => {
         else{
             meeting = props.meetingTimes[0];
         }
-        return <TextWithIcon title="Meeting Times" classname={styles.greytext}
+        return <TextWithIcon title="Meeting Time" classname={styles.greytext}
                 text={convertMeetingTime(meeting)}
                 icon={FaUserClock}/>;
     }
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const sorted: MeetingTime[][] = Array(7).fill(0).map(x => Array());
+    props.meetingTimes.forEach(meeting => {
+        let index = 0;
+        let dayTimes = sorted[meeting.day];
+        for (; index < dayTimes.length; index++){
+            if (meeting.startTime < dayTimes[index].startTime){
+                break;
+            }
+        }
+        dayTimes.splice(index, 0, meeting);
+    }); // efficiently sorts the MeetingTimes into bins by day, and then by start time
+
     return (<><a className={styles.underlineLinkHover} onClick={onOpen}>
-        <TextWithIcon classname={styles.greytext} icon={FaUserClock} text="Multiple Meeting Times"/>
+        <TextWithIcon classname={styles.greytext} icon={FaUserClock} text="Multiple Meeting Times" title="Meeting Times"/>
 
         </a>
-        <Modal isOpen={isOpen} onClose={onClose}>
+        <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="inside" size="lg">
             <ModalOverlay/>
             <ModalContent>
-                <ModalHeader>Meeting Times</ModalHeader>
+                <ModalHeader alignSelf="center" style={{textDecoration: "underline"}}>Meeting Times</ModalHeader>
                 <ModalCloseButton/>
                 <ModalBody>
-                    CONTENT
+                    <VStack alignItems="normal">
+                        {sorted.map((dayTimes, index) => 
+                            dayTimes.length === 0 ? <></> : 
+                            <VStack alignItems="normal">
+                                <Text fontWeight="bold">{Day[index]}:</Text>
+                                <UnorderedList paddingLeft="30px">
+                                    {dayTimes.map((meetingTime, i) => 
+                                        // key attribute is not working, need to fix
+                                        <ListItem>
+                                        <VStack alignItems="normal" spacing="0">
+                                            <Text>{meetingTime.name}</Text>
+                                            <Text paddingLeft="10px">{
+                                            convertTimeRange(meetingTime.startTime, meetingTime.endTime) +
+                                            ", " + Frequency[meetingTime.frequency]}</Text>
+                                        </VStack>
+                                        </ListItem>)}
+                                </UnorderedList>
+                            </VStack>
+                        )}
+                    </VStack>
                 </ModalBody>
                 <ModalFooter/>
             </ModalContent>
--- src/stringConverters.ts
+++ src/stringConverters.ts
@@ -17,12 +17,14 @@ export function convertRange(range: Range): string{
 }
 export function convertMeetingTime(meeting: MeetingTime): string{
     return Day[meeting.day] + " " + 
-    convertTime(meeting.startTime) + " - " +
-    convertTime(meeting.endTime) + ", " +
+    convertTimeRange(meeting.startTime, meeting.endTime) + ", " +
     Frequency[meeting.frequency];
 }
 export function convertTime(time: number): string{
     const hour = Math.trunc(time / 60);
     const min = Math.trunc(time % 60);
     return (hour < 10 ? "0" + hour : hour) + ":" + (min < 10 ? "0" + min : min);
+}
+export function convertTimeRange(time1: number, time2: number){
+    return `${convertTime(time1)} - ${convertTime(time2)}`;
 }
