c9d2bdbaa9ae5f5786f3b055c0e3fa234f130838
EdwardW <02ewatine@gmail.com>
2021-03-15 20:51:42 +0000

    Add a button. That floats. *Quack*

---
+++ src/components/groupComponents/FloatingButton.tsx
@@ -0,0 +1,9 @@
+
+import { IconButton, IconButtonProps } from "@chakra-ui/react"
+
+const FloatingButton = (params: IconButtonProps) => {
+    return <IconButton width="70px" height="70px" borderRadius="full" backgroundPosition="top"
+        position="fixed" bottom="10px" right="10px" {...params}/>
+}
+
+export default FloatingButton;
