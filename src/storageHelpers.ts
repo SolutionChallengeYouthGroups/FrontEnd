abaf4347bfb187848c1cee95c5566de6cf035992
hugo-wb <hugowong71@gmail.com>
2021-03-22 12:27:22 +0000

    nvm

---
+++ src/storageHelpers.ts
@@ -0,0 +1,13 @@
+import { storage } from "./firebase";
+
+export function storagePathToURL(path: string, args: string): string {
+    return (
+        "https://firebasestorage.googleapis.com/v0/b/soltuionchallenge.appspot.com/o/" +
+        encodeURIComponent(path) +
+        "?" +
+        args
+    );
+}
+export async function getGroupAvatarURL(groupID: string){
+    return storagePathToURL("groups/" + groupID + "/profile.png", "alt=media");
+}
