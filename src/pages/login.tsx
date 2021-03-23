84a2665bc6d041c9e4d0498fb47c1b9caddd849d
Hugo Wong-Berard <hugowong71@gmail.com>
2021-03-23 16:30:59 +0000

    Feat/nav bar+home page (#12)
    * branch init
    
    * Feat: updated homepage+navbar
    
    HomePage:
    updated it so that it shows a list of groups, each group is its own
    card, whole thing is relatively responsive
    
    NavBar:
    fixed the style a bit
    
    * Previous commit clean up
    
    probably shouldve done this in the previuos commit :(
    Updated the navbar icon and made the age range at the bottom work
    
    * some more minor fixes
    
    Not sure if I shoudl use firebase.auth() or useContext for the user
    navbar
    Added some animations
    
    * more polishing of the UI
    
    added mroe green, fixed some of the alyout and made the box shadow
    better
    
    * Final comments and formatting
    
    Added some comments and formatted a couple docuemnts to 4 spaces :(

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,119 +10,133 @@ import TextInput from "../components/forms/TextInput";
 interface Props {}
 
 const login = () => {
-  // page to login
-  const router = useRouter();
+    // page to login
+    const router = useRouter();
 
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      router.push("./");
-    }
-  });
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            router.push("./");
+        }
+    });
 
-  const signInWithGoogle = () => {
-    // sign up with google
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(() => {
-        // continue to home page
-        router.push("./");
-      })
-      .catch((error) => {
-        firebase.auth().signInWithRedirect(provider);
-      });
-  };
-
-  const signInAnonymously = () => {
-    firebase
-      .auth()
-      .signInAnonymously()
-      .then(() => {
-        router.push("./");
-      })
-      .catch((error) => console.log(error));
-  };
-
-  return (
-    <Flex w="100vw" h="100vh" justifyContent="center" alignItems="center">
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        validateOnChange={false}
-        validateOnBlur={false}
-        onSubmit={async (values, actions) => {
-          let { email, password } = values;
-          firebase
+    const signInWithGoogle = () => {
+        // sign up with google
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase
             .auth()
-            .signInWithEmailAndPassword(email, password)
+            .signInWithPopup(provider)
             .then(() => {
-              router.push("./");
+                // continue to home page
+                router.push("./");
             })
             .catch((error) => {
-              switch (error.code) {
-                case "auth/invalid-email":
-                  actions.setFieldError("email", "Invalid Email");
-                  break;
-                case "auth/wrong-password":
-                  actions.setFieldError("password", "Wrong Password");
-              }
+                firebase.auth().signInWithRedirect(provider);
             });
-        }}
-        validate={(values) => {
-          let errors: any = {};
-          if (!isEmail(values.email)) {
-            errors.email = "Invalid email adress";
-          }
-          return errors;
-        }}
-      >
-        <Form style={{ width: "100%", maxWidth: "300px" }}>
-          <VStack w="100%">
-            <Heading as="h1" size="xl" pb="10px" mb="20px" borderBottom="1px">
-              Login
-            </Heading>
-            <TextInput
-              name="email"
-              type="text"
-              label="Email"
-              placeholder="john.doe@gmail.com"
-              required
-            />
-            <TextInput
-              name="password"
-              type="password"
-              label="Password"
-              placeholder="secretPassword123"
-              required
-            />
-            <Button w="100%" type="submit">
-              Login
-            </Button>
-            <Button
-              w="100%"
-              leftIcon={<Icon as={FaGoogle} />}
-              variant="solid"
-              colorScheme="blue"
-              onClick={signInWithGoogle}
+    };
+
+    const signInAnonymously = () => {
+        firebase
+            .auth()
+            .signInAnonymously()
+            .then(() => {
+                router.push("./");
+            })
+            .catch((error) => console.log(error));
+    };
+
+    return (
+        <Flex w="100vw" h="100vh" justifyContent="center" alignItems="center">
+            <Formik
+                initialValues={{
+                    email: "",
+                    password: "",
+                }}
+                validateOnChange={false}
+                validateOnBlur={false}
+                onSubmit={async (values, actions) => {
+                    let { email, password } = values;
+                    firebase
+                        .auth()
+                        .signInWithEmailAndPassword(email, password)
+                        .then(() => {
+                            router.push("./");
+                        })
+                        .catch((error) => {
+                            switch (error.code) {
+                                case "auth/invalid-email":
+                                    actions.setFieldError(
+                                        "email",
+                                        "Invalid Email"
+                                    );
+                                    break;
+                                case "auth/wrong-password":
+                                    actions.setFieldError(
+                                        "password",
+                                        "Wrong Password"
+                                    );
+                            }
+                        });
+                }}
+                validate={(values) => {
+                    let errors: any = {};
+                    if (!isEmail(values.email)) {
+                        errors.email = "Invalid email adress";
+                    }
+                    return errors;
+                }}
             >
-              Login with Google
-            </Button>
-            <Button
-              w="100%"
-              leftIcon={<Icon as={FaUserSecret} />}
-              variant="incognito"
-              onClick={signInAnonymously}
-            >
-              Login Anonymously
-            </Button>
-          </VStack>
-        </Form>
-      </Formik>
-    </Flex>
-  );
+                <Form style={{ width: "100%", maxWidth: "300px" }}>
+                    <VStack w="100%">
+                        <Heading
+                            as="h1"
+                            size="xl"
+                            pb="10px"
+                            mb="20px"
+                            px="8px"
+                            borderBottom="2px solid"
+                            borderColor="main"
+                        >
+                            Login
+                        </Heading>
+                        <TextInput
+                            name="email"
+                            type="text"
+                            label="Email"
+                            placeholder="john.doe@gmail.com"
+                            required
+                        />
+                        <TextInput
+                            name="password"
+                            type="password"
+                            label="Password"
+                            placeholder="secretPassword123"
+                            required
+                        />
+                        <Button w="100%" type="submit">
+                            Login
+                        </Button>
+                        <Button
+                            w="100%"
+                            leftIcon={<Icon as={FaGoogle} />}
+                            variant="solid"
+                            colorScheme="blue"
+                            onClick={signInWithGoogle}
+                        >
+                            Login with Google
+                        </Button>
+                        <Button
+                            w="100%"
+                            leftIcon={<Icon as={FaUserSecret} />}
+                            variant="incognito"
+                            onClick={signInAnonymously}
+                        >
+                            Login Anonymously
+                        </Button>
+                    </VStack>
+                </Form>
+            </Formik>
+        </Flex>
+    );
 };
 
 export default login;
