42ef244def1e4523b8b4ebac22fbcc5a7d2186c9
hugo-wb <hugowong71@gmail.com>
2021-03-27 19:56:04 +0000

    fix

--- src/components/groupComponents/GroupCategoryDisplay.tsx
+++ src/components/groupComponents/GroupCategoryDisplay.tsx
@@ -7,38 +7,64 @@ import { HTMLProps } from "react";
 import { HStack, Icon, Select } from "@chakra-ui/react";
 import React, { useState } from "react";
 
-
 interface Props extends HTMLProps<React.ReactFragment> {
-  group: Group;
-  edit?: boolean;
+    group: Group;
+    edit?: boolean;
 }
-const GroupCategoryDisplay = ({group, edit, ...rest}: Props) => {
+const GroupCategoryDisplay = ({ group, edit, ...rest }: Props) => {
     const [category, setCategoryState] = useState(group.category);
-    
-    function setCategory(category: GroupCategory){
-      group.category = category;
-      setCategoryState(category);
+
+    function setCategory(category: GroupCategory) {
+        group.category = category;
+        setCategoryState(category);
     }
-    if (group.category === "" && edit !== true){
-        return <React.Fragment {...rest}/>;
+    if (group.category == "" && edit !== true) {
+        return <React.Fragment {...rest} />;
     }
-    if (edit !== undefined){
-      return <React.Fragment {...rest}>
-      <TextWithIcon title="Group Category" className={styles.greytext} display={edit ? "none" : "flex"}
-      text={title(group.category)} icon={groupCategoryMapping.get(group.category)}/>
-      <HStack display={edit ? "flex" : "none"}>
-        {group.category === "" ? <></> : <Icon as={groupCategoryMapping.get(group.category)}/>}
-        <Select value={group.category} variant="outline" placeholder="Group category"
-        onChange={(e) => setCategory(e.target.value as GroupCategory)}>
-          {GroupCategories.map((gc) => 
-          <option key={gc} value={gc}>{title(gc)}</option>)}
-        </Select>
-      </HStack>
-      </React.Fragment>
+    if (edit !== undefined) {
+        return (
+            <React.Fragment {...rest}>
+                <TextWithIcon
+                    title="Group Category"
+                    className={styles.greytext}
+                    display={edit ? "none" : "flex"}
+                    text={title(group.category)}
+                    icon={groupCategoryMapping.get(group.category)}
+                />
+                <HStack display={edit ? "flex" : "none"}>
+                    {group.category != "" ? (
+                        <></>
+                    ) : (
+                        <Icon as={groupCategoryMapping.get(group.category)} />
+                    )}
+                    <Select
+                        value={group.category}
+                        variant="outline"
+                        placeholder="Group category"
+                        onChange={(e) =>
+                            setCategory(e.target.value as GroupCategory)
+                        }
+                    >
+                        {GroupCategories.map((gc) => (
+                            <option key={gc} value={gc}>
+                                {title(gc)}
+                            </option>
+                        ))}
+                    </Select>
+                </HStack>
+            </React.Fragment>
+        );
     }
-    return <React.Fragment {...rest}><TextWithIcon title="Group Category" className={styles.greytext} 
-    text={title(group.category)} icon={groupCategoryMapping.get(group.category)}/>
-    </React.Fragment>
+    return (
+        <React.Fragment {...rest}>
+            <TextWithIcon
+                title="Group Category"
+                className={styles.greytext}
+                text={title(group.category)}
+                icon={groupCategoryMapping.get(group.category)}
+            />
+        </React.Fragment>
+    );
 };
 
 export default GroupCategoryDisplay;
--- src/firestoreTypes.ts
+++ src/firestoreTypes.ts
@@ -1,119 +1,156 @@
 // firestore data types
 import {
-  DocumentReference,
-  CollectionReference,
-  GeoPoint,
-  Timestamp,
+    DocumentReference,
+    CollectionReference,
+    GeoPoint,
+    Timestamp,
 } from "@firebase/firestore-types";
-import firebase from "./firebase"
+import firebase from "./firebase";
 import { Ref } from "typesaurus";
 import { Coords } from "google-map-react";
 
 interface User {
-  name: string;
-  username: string;
-  email: string;
-  groups: Ref<Group>[]; // reference to Groups this user is part of
-  createdAt: Timestamp;
-  phoneNumber?:string; // optional phone number, probz just for adults
-  // profile picture is the userId
+    name: string;
+    username: string;
+    email: string;
+    groups: Ref<Group>[]; // reference to Groups this user is part of
+    createdAt: Timestamp;
+    phoneNumber?: string; // optional phone number, probz just for adults
+    // profile picture is the userId
 }
 
-type GroupCategory = "scouting" | "physical" | "educational" | "social" | "artistic" | "board games" |
-                  "gaming" | "faith-based" | "political" | "other";
-export const GroupCategories = ["scouting", "physical", "educational", "social", "artistic", "board games",
-"gaming", "faith-based", "political", "other"]; // create a list for looping
+type GroupCategory =
+    | "scouting"
+    | "physical"
+    | "educational"
+    | "social"
+    | "artistic"
+    | "board games"
+    | "gaming"
+    | "faith-based"
+    | "political"
+    | "other"
+    | "";
+export const GroupCategories = [
+    "scouting",
+    "physical",
+    "educational",
+    "social",
+    "artistic",
+    "board games",
+    "gaming",
+    "faith-based",
+    "political",
+    "other",
+]; // create a list for looping
 
 interface Group {
-  // the ID is not the same as the name to avoid checking duplicates
-  name: string;
-  location?: GeoPoint;
-  description: string;
-  category: GroupCategory;
-  createdAt: Timestamp;
-  owners: Ref<User>[];
-  links: SocialLinks;
-  ageRange: Range;
-  meetingTimes: MeetingTime[];
-  // group picture is the id
+    // the ID is not the same as the name to avoid checking duplicates
+    name: string;
+    location?: GeoPoint;
+    description: string;
+    category: GroupCategory;
+    createdAt: Timestamp;
+    owners: Ref<User>[];
+    links: SocialLinks;
+    ageRange: Range;
+    meetingTimes: MeetingTime[];
+    // group picture is the id
 }
 
-interface Range{ // for ages, zero is a signifier for a missing value
-  min: number;
-  max: number;
+interface Range {
+    // for ages, zero is a signifier for a missing value
+    min: number;
+    max: number;
 }
 
-interface SocialLinks{
-  email: string;
-  facebook: string;
-  instagram: string;
-  phone: string;
-  twitter: string;
-  website: string;
-  whatsapp: string;
+interface SocialLinks {
+    email: string;
+    facebook: string;
+    instagram: string;
+    phone: string;
+    twitter: string;
+    website: string;
+    whatsapp: string;
 }
 
-export enum Day{ // the entries are ordered
-  Monday,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday,
-  Sunday
-};
-
-interface MeetingTime{
-  name: string,
-  day: Day,
-  startTime: number, // minutes past midnight
-  endTime: number,   // minutes past midnight as well
-  frequency: Frequency
+export enum Day { // the entries are ordered
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
 }
 
-export enum Frequency{ // the entries are ordered
-  weekly,
-  fortnightly,
-  monthly
+interface MeetingTime {
+    name: string;
+    day: Day;
+    startTime: number; // minutes past midnight
+    endTime: number; // minutes past midnight as well
+    frequency: Frequency;
+}
+
+export enum Frequency { // the entries are ordered
+    weekly,
+    fortnightly,
+    monthly,
 }
 
 interface Post {
-  title: string;
-  author: Ref<User>;
-  createdAt: Timestamp;
-  content: string;
-  comments: CollectionReference;
+    title: string;
+    author: Ref<User>;
+    createdAt: Timestamp;
+    content: string;
+    comments: CollectionReference;
 }
 interface Comment {
-  author: Ref<User>; // reference to author
-  createdAt: Timestamp;
-  content: string;
+    author: Ref<User>; // reference to author
+    createdAt: Timestamp;
+    content: string;
 }
 
-export class GeoPointLocation{
-  lat: number;
-  lon: number;
-  constructor (lat: number, lon: number){
-    this.lat = lat;
-    this.lon = lon;
-  }
-  static fromGeoPoint(geoPoint: firebase.firestore.GeoPoint): GeoPointLocation{
-    // @ts-ignore
-    return new GeoPointLocation(geoPoint.x_, geoPoint.N_);
-  }
-  toGeoPoint(): firebase.firestore.GeoPoint{
-    return new firebase.firestore.GeoPoint(this.lat, this.lon);
-  }
-  toMapCoords(): Coords{
-    return {
-      lat: this.lat,
-      lng: this.lon
+export class GeoPointLocation {
+    lat: number;
+    lon: number;
+    constructor(lat: number, lon: number) {
+        this.lat = lat;
+        this.lon = lon;
     }
-  }
-  equals(geoPointLocation: GeoPointLocation, tolerance: number = 1e-8): boolean{
-    return Math.abs(this.lat - geoPointLocation.lat) < tolerance &&
-    Math.abs(this.lon - geoPointLocation.lon) < tolerance;
-  }
+    static fromGeoPoint(
+        geoPoint: firebase.firestore.GeoPoint
+    ): GeoPointLocation {
+        // @ts-ignore
+        return new GeoPointLocation(geoPoint.x_, geoPoint.N_);
+    }
+    toGeoPoint(): firebase.firestore.GeoPoint {
+        return new firebase.firestore.GeoPoint(this.lat, this.lon);
+    }
+    toMapCoords(): Coords {
+        return {
+            lat: this.lat,
+            lng: this.lon,
+        };
+    }
+    equals(
+        geoPointLocation: GeoPointLocation,
+        tolerance: number = 1e-8
+    ): boolean {
+        return (
+            Math.abs(this.lat - geoPointLocation.lat) < tolerance &&
+            Math.abs(this.lon - geoPointLocation.lon) < tolerance
+        );
+    }
 }
 
-export type { Group, User, GroupCategory, SocialLinks , Range, MeetingTime, Post, Comment };
+export type {
+    Group,
+    User,
+    GroupCategory,
+    SocialLinks,
+    Range,
+    MeetingTime,
+    Post,
+    Comment,
+};
