ea8af2f5e0e98750c55b770730db81cad47a20a7
hugo-wb <hugowong71@gmail.com>
2021-03-27 10:42:11 +0000

    branch init

---
+++ src/pages/map.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const map = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default map
