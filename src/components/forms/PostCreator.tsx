17d0c37427e3f8edd5b98949d5f978c0934cbb78
hugo-wb <hugowong71@gmail.com>
2021-03-28 12:26:59 +0100

    Implemented basic post creation
    Very basic functionality, might switch to formik to do all the
    validation etc...

--- src/components/forms/PostCreator.tsx
+++ src/components/forms/PostCreator.tsx
@@ -1,47 +1,141 @@
-import { Flex, Icon, Input, Stack, Textarea } from "@chakra-ui/react";
-import React, { createRef, MouseEventHandler, useRef } from "react";
+import {
+    Button,
+    Flex,
+    Text,
+    HStack,
+    Icon,
+    Input,
+    Spacer,
+    Stack,
+    Textarea,
+} from "@chakra-ui/react";
+import React, { createRef, MouseEventHandler, useRef, useState } from "react";
 import { AiFillFileImage } from "react-icons/ai";
+import { add } from "typesaurus";
+import { posts, users } from "../../firestoreCollections";
+import { Post } from "../../firestoreTypes";
+import { getCurrentUserRef, uploadPost } from "../../storageHelpers";
+import firebase from "firebase";
+import { IoConstructOutline } from "react-icons/io5";
 
 interface Props {}
 
-
-
 const PostCreator = (props: Props) => {
     let fileInput: HTMLInputElement | null = null;
-    const [image,setImage] = useState()
+    const [image, setImage] = useState<File>();
+    const [post, setPost] = useState<{
+        title: string;
+        content: string;
+        image?: File;
+    }>({ title: "", content: "" });
 
     const callUploadImage = () => {
         if (fileInput) {
             fileInput.click();
         }
     };
-    const handleImageUpload = (
-        e: React.MouseEvent<HTMLInputElement, MouseEvent>
-    ) => {
-        if (e.currentTarget.files){
-
+    const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.currentTarget.files) {
+            setPost({ ...post, image: e.currentTarget.files[0] });
         }
     };
 
+    const submit = async () => {
+        let userRef = getCurrentUserRef();
+        if (!userRef) {
+            return;
+        }
+        let newPost = await add(posts, {
+            author: userRef,
+            content: post.content,
+            title: post.title,
+            createdAt: firebase.firestore.Timestamp.now(),
+        });
+        if (post.image) {
+            uploadPost(post.image, newPost.id);
+        }
+    };
+    console.log(post);
+
+    const handleChange = (e: any) => {
+        setPost({
+            ...post,
+            [e.target.name]: e.target.value,
+        });
+    };
     return (
-        <Flex>
-            <Textarea placeholder="Write new post" />
-            <Stack>
-                <Input
+        <Flex
+            flexDir="column"
+            justifyContent="space-around"
+            alignItems="flex-start"
+            w="100%"
+            paddingX="40px"
+            paddingY="10px"
+            _after={{
+                content: `""`,
+                borderBottom: "1px solid",
+                borderColor: "main",
+                w: "100%",
+            }}
+        >
+            <Input
+                placeholder="Title"
+                w="100%"
+                marginY="10px"
+                name="title"
+                onChange={handleChange}
+            />
+            <Textarea
+                placeholder="Write new post"
+                w="100%"
+                resize="none"
+                name="content"
+                onChange={handleChange}
+            />
+            <Flex
+                flexDir="row"
+                justifyContent="flex-start"
+                alignItems="center"
+                w="100%"
+                marginY="10px"
+            >
+                <input
                     type="file"
                     hidden={true}
                     ref={(input) => {
                         fileInput = input;
                     }}
-                    onClick={handleImageUpload}
+                    onChange={handleImageUpload}
                 />
+                {/* <Input
+                    type="file"
+                    hidden={true}
+                    ref={(input) => {
+                        fileInput = input;
+                    }}
+                    onChange={handleImageUpload}
+                /> */}
                 <Icon
                     as={AiFillFileImage}
                     color="main"
                     onClick={callUploadImage}
-                    _hover={{ cursor: "pointer" }}
+                    _hover={{ cursor: "pointer", color: "mainDark" }}
+                    w="30px"
+                    h="auto"
+                    marginX="15px"
                 />
-            </Stack>
+                <Text>{image?.name}</Text>
+                <Spacer />
+                <Button
+                    variant="solid"
+                    backgroundColor="main"
+                    textColor="pureWhite"
+                    _hover={{ backgroundColor: "mainDark" }}
+                    onClick={submit}
+                >
+                    Post
+                </Button>
+            </Flex>
         </Flex>
     );
 };
--- src/storageHelpers.ts
+++ src/storageHelpers.ts
@@ -1,11 +1,37 @@
-import firebase from "firebase"
-export function storagePathToURL(path: string, args: string): string{
-    return "https://firebasestorage.googleapis.com/v0/b/soltuionchallenge.appspot.com/o/"+
-    encodeURIComponent(path)+"?"+args;
+import firebase from "firebase";
+import { Ref, ref } from "typesaurus";
+import { auth } from "./firebase";
+import { users } from "./firestoreCollections";
+import { User } from "./firestoreTypes";
+export function storagePathToURL(path: string, args: string): string {
+    return (
+        "https://firebasestorage.googleapis.com/v0/b/soltuionchallenge.appspot.com/o/" +
+        encodeURIComponent(path) +
+        "?" +
+        args
+    );
 }
-export function getGroupAvatarURL(groupID: string): string{
-    return storagePathToURL("groups/"+groupID+"/profile.png", "alt=media");
+export function getGroupAvatarURL(groupID: string): string {
+    return storagePathToURL("groups/" + groupID + "/profile.png", "alt=media");
 }
-export function setGroupAvatar(file: Blob, groupID: string): firebase.storage.UploadTask{
-    return firebase.storage().ref("/groups/"+groupID+"/profile.png").put(file);
+export function setGroupAvatar(
+    file: Blob,
+    groupID: string
+): firebase.storage.UploadTask {
+    return firebase
+        .storage()
+        .ref("/groups/" + groupID + "/profile.png")
+        .put(file);
+}
+
+export function uploadPost(file: File, postID: string) {
+    let ref = firebase.storage().ref(`/posts/${postID}/${file.name}`);
+    return ref.put(file, { contentType: file.type });
+}
+
+export function getCurrentUserRef(): Ref<User> | undefined {
+    let user = firebase.auth().currentUser;
+    if (user) {
+        return ref(users, user.uid);
+    }
 }
