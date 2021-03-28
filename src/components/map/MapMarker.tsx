0cfd5cf4a9dcd29104d97dcb65d4c7713ca95520
EdwardW <02ewatine@gmail.com>
2021-03-28 22:30:50 +0100

    Add shadow to mapMarker. Rework popup.

--- src/components/map/MapMarker.tsx
+++ src/components/map/MapMarker.tsx
@@ -1,6 +1,7 @@
 import {
     Box,
     ColorProps,
+    HStack,
     Icon,
     IconProps,
     Popover,
@@ -11,7 +12,7 @@ import {
     PopoverHeader,
     PopoverTrigger,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { FaMapMarker } from "react-icons/fa";
 import GroupCard from "../cards/GroupCard";
 
@@ -24,33 +25,78 @@ interface Props extends IconProps {
 
 const MapMarker = ({ popup, isOpen, color, ...rest }: Props) => {
     // takes in a popup and will create a map maker, when clicked on will show the popup
+    const focusRef = useRef(null);
     return popup ? (
-        <Popover defaultIsOpen={!!isOpen} placement="top-end">
+        <Popover
+            defaultIsOpen={!!isOpen}
+            placement="top-end"
+            initialFocusRef={focusRef}
+        >
             <PopoverTrigger>
-                <Icon
-                    transform="translate(-50%, -100%);"
-                    as={FaMapMarker}
-                    color={color || "main"}
-                    h="34px"
-                    w="auto"
-                    _hover={{ cursor: "pointer", filter: "brightness(0.8)" }}
-                    marginTop="-17px"
-                    {...rest}
-                />
+                <Box marginTop="-17px">
+                    <Icon
+                        transform="translate(-50%, -100%);"
+                        filter="blur(2px);"
+                        as={FaMapMarker}
+                        color="black"
+                        h="34px"
+                        w="auto"
+                        position="absolute"
+                        top="1px"
+                        left="1px"
+                    />
+                    <Icon
+                        h="34px"
+                        w="auto"
+                        transform="translate(-50%, -100%);"
+                        as={FaMapMarker}
+                        color={color || "main"}
+                        _hover={{
+                            cursor: "pointer",
+                            filter: "brightness(0.8)",
+                        }}
+                        position="absolute"
+                        top="0px"
+                        left="0px"
+                        {...rest}
+                    />
+                </Box>
             </PopoverTrigger>
-            <PopoverContent w="300px">
-                <PopoverBody>{popup}</PopoverBody>
+            <PopoverContent
+                bg="transparent"
+                minWidth="1000px"
+                border="transparent"
+            >
+                <HStack>
+                    <Box ref={focusRef}>{popup}</Box>
+                </HStack>
             </PopoverContent>
         </Popover>
     ) : (
-        <Icon
-            transform="translate(-50%, -100%);"
-            as={FaMapMarker}
-            color={color || "main"}
-            w="35px"
-            h="35px"
-            {...rest}
-        />
+        <Box marginTop="-17px">
+            <Icon
+                transform="translate(-50%, -100%);"
+                filter="blur(2px);"
+                as={FaMapMarker}
+                color="black"
+                h="34px"
+                w="auto"
+                position="absolute"
+                top="1px"
+                left="1px"
+            />
+            <Icon
+                h="34px"
+                w="auto"
+                transform="translate(-50%, -100%);"
+                as={FaMapMarker}
+                color={color || "main"}
+                position="absolute"
+                top="0px"
+                left="0px"
+                {...rest}
+            />
+        </Box>
     );
 };
 
