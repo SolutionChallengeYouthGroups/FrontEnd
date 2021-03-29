3d5d779329462e28334644122e541b89d256deba
Justin Tan How Yuan <justanhowyuan@gmail.com>
2021-03-29 20:30:33 +0800

    Now age range will only change automatically when input loses focus (#36)
    * Now age range will only change automatically when input loses focus
    
    as supposed to when the input changes.
    
    * Patched age range max not allowing 0 as an undefined value.

--- src/components/pages/GroupPage.tsx
+++ src/components/pages/GroupPage.tsx
@@ -40,8 +40,6 @@ import { groups, users } from "../../firestoreCollections";
 import _ from "lodash";
 import { useRouter } from "next/router";
 
-import { useContext } from "react";
-import { UserContext } from "../../lib/context";
 import GroupAvatar from "../groupComponents/GroupAvatar";
 
 interface Props {
